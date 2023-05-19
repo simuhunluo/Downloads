f95261c6361e7c17889c3a5e150d661e0bf083a1
mundane <zfyoung799699@gmail.com>
2023-05-19 22:53:38 +0800

    修改readme，修改cookie

--- README.md
+++ README.md
@@ -1,18 +1,46 @@
-# Downloads 抖音下载
+# 抖音下载
 
 ### 简介
 
 一款可以下载无水印抖音视频/图片的安卓app
 
-### 功能特色
+### apk下载
 
+https://wwbl.lanzout.com/b03p8kx6b
+密码:8888
+
+### 功能特色
 1. 支持主页批量解析
 2. 没有服务端，解析功能全部在客户端实现
 
 ### 功能截图
-
 <p float="left">
      <img src="./screenshots/1.jpeg" width="200"/>
      <img src="./screenshots/2.jpeg" width="200"/>
      <img src="./screenshots/3.jpeg" width="200"/>
 </p>
+<p float="left">
+     <img src="./screenshots/4.jpeg" width="200"/>
+     <img src="./screenshots/5.jpeg" width="200"/>
+     <img src="./screenshots/6.jpeg" width="200"/>
+</p>
+
+### 免责声明
+1. 本软件只提供视频解析，不提供任何资源上传、存储到服务器的功能。
+2. 本软件仅解析来自 抖音的内容，不会对解析到的音视频进行二次编码，部分视频会进行有限的格式转换、拼接等操作。
+3. 本软件解析得到的所有内容均来自抖音用户上传、分享，其版权均归原作者所有。内容提供者、上传者应对其提供、上传的内容承担全部责任。
+4. **本软件提供的所有内容，仅可用作学习交流使用，未经原作者授权，禁止用于其他用途。请在下载 24 小时内删除。为尊重作者版权，请前往资源的原始发布网站观看，支持原创，谢谢。**
+5. 因使用本软件产生的版权问题，软件作者概不负责。
+
+### 打赏作者
+
+<p float="left">
+     <img src="./screenshots/reward_alipay.jpeg" width="200"/>
+     <img src="./screenshots/reward_wx.jpeg" width="200"/>
+</p>
+
+
+### 参考项目
+<https://gitee.com/lcl0512/douyin-downloader>
+
+
--- app/src/main/java/com/mundane/downloads/ui/AboutMeActivity.java
+++ app/src/main/java/com/mundane/downloads/ui/AboutMeActivity.java
@@ -1,21 +1,14 @@
 package com.mundane.downloads.ui;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
-import android.widget.ImageView;
 import com.mundane.downloads.R;
-import com.mundane.downloads.util.SaveImageUtils;
-import com.mundane.downloads.util.T;
 
 public class AboutMeActivity extends AppCompatActivity {
     
-    private ImageView mIvAuthorWx;
     private View mCvReward;
     
     @Override
@@ -23,15 +16,7 @@ public class AboutMeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
         
-        mIvAuthorWx = findViewById(R.id.iv_author_wx);
         mCvReward = findViewById(R.id.cv_reward);
-        mIvAuthorWx.setOnLongClickListener(new OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                saveImage();
-                return true;
-            }
-        });
         mCvReward.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,12 +30,4 @@ public class AboutMeActivity extends AppCompatActivity {
         startActivity(intent);
     }
     
-    private void saveImage() {
-        Bitmap bitmap = ((BitmapDrawable) mIvAuthorWx.getDrawable()).getBitmap();
-        if (SaveImageUtils.saveImageToGallery(this, bitmap)) {
-            T.show("图片保存成功");
-        } else {
-            T.show("图片保存失败");
-        }
-    }
 }
